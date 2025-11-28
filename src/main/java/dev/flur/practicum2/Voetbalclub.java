ecc7324b8245cb85da7eb33bb3abb4f0b202cc46
flurbudurbur <69259138+flurbudurbur@users.noreply.github.com>
2025-11-28 21:33:17 +0100

    p2: voetbalclub

---
+++ src/main/java/dev/flur/practicum2/Voetbalclub.java
@@ -0,0 +1,62 @@
+package dev.flur.practicum2;
+
+public class Voetbalclub {
+    private String naam;
+    private int aantalGewonnen;
+    private int aantalGelijk;
+    private int aantalVerloren;
+    private int score;
+
+    public Voetbalclub(String naam) {
+        naam = (naam != null) ? naam.trim() : "";
+        this.naam = (naam.isEmpty()) ? "FC" : naam;
+    }
+
+    public Voetbalclub() {
+        this("FC");
+    }
+
+    public void verwerkResultaat(char rslt) {
+        switch (rslt) {
+            case 'w':
+                this.aantalGewonnen++;
+                this.score += 3;
+                break;
+            case 'v':
+                this.aantalVerloren++;
+                break;
+            case 'g':
+                this.aantalGelijk++;
+                this.score++;
+                break;
+        }
+    }
+
+    public int getAantalVerloren() {
+        return aantalVerloren;
+    }
+
+    public int getAantalGelijk() {
+        return aantalGelijk;
+    }
+
+    public int getAantalGewonnen() {
+        return aantalGewonnen;
+    }
+
+    public String getNaam() {
+        return this.naam;
+    }
+
+    public int aantalPunten() {
+        return this.score;
+    }
+
+    public int aantalGespeeld() {
+        return aantalVerloren + aantalGelijk + aantalGewonnen;
+    }
+
+    public String toString() {
+        return naam + " " + aantalGespeeld() + " " + aantalGewonnen + " " + aantalGelijk + " " + aantalVerloren + " " + aantalPunten();
+    }
+}
