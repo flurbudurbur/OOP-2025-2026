a6605afeebc82f0cbc8d8ec7221a430372c066bd
flurbudurbur <69259138+flurbudurbur@users.noreply.github.com>
2026-01-16 10:14:30 +0100

    P2a: Main en Zwembad

---
+++ src/main/java/dev/flur/practicum2/Zwembad.java
@@ -0,0 +1,50 @@
+package dev.flur.practicum2;
+
+public class Zwembad {
+    private double breedte;
+    private double lengte;
+    private double diepte;
+
+    public Zwembad(double breedte, double lengte, double diepte) {
+        this.breedte = breedte;
+        this.lengte = lengte;
+        this.diepte = diepte;
+    }
+
+    public Zwembad() {
+        this(0.0, 0.0, 0.0);
+    }
+
+    public double getLengte() {
+        return lengte;
+    }
+
+    public void setLengte(double lengte) {
+        this.lengte = lengte;
+    }
+
+    public void setDiepte(double diepte) {
+        this.diepte = diepte;
+    }
+
+    public double getDiepte() {
+        return diepte;
+    }
+
+    public double getBreedte() {
+        return breedte;
+    }
+
+    public void setBreedte(double breedte) {
+        this.breedte = breedte;
+    }
+
+    public double inhoud() {
+        return breedte * lengte * diepte;
+    }
+
+    @Override
+    public String toString() {
+        return "Dit zwembad is " + breedte + " meter breed, " + lengte + " meter lang, en " + diepte + " meter diep";
+    }
+}
